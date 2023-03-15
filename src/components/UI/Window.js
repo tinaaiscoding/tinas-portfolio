7ad1e14114d328cb79b9d6892c6ff883c186fccd
tinanvo <tinanhivo@gmail.com>
2023-03-16 10:19:38 +1100

    Windows are draggable, repositioned AboutMe & Resume window

--- src/components/UI/Window.js
+++ src/components/UI/Window.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import Draggable from 'react-draggable';
+
 import classes from './Window.module.css';
 import 'primeicons/primeicons.css';
 
 const Window = (props) => {
   return (
-    <div className={`${classes.window} ${props.className}`}>
-      <div className={classes.actionBar}>
-        <span
-          className={`pi pi-circle-fill ${classes.red} ${classes.circleFill}`}
-        ></span>
-        <span
-          className={`pi pi-circle-fill ${classes.amber} ${classes.circleFill}`}
-        ></span>
-        <span
-          className={`pi pi-circle-fill ${classes.green} ${classes.circleFill}`}
-        ></span>
+    <Draggable handle='.actionBar'>
+      <div className={`${classes.window} ${props.className}`}>
+        <div className={`${classes.actionBar} actionBar`}>
+          <span
+            className={`pi pi-circle-fill ${classes.red} ${classes.circleFill}`}
+          ></span>
+          <span
+            className={`pi pi-circle-fill ${classes.amber} ${classes.circleFill}`}
+          ></span>
+          <span
+            className={`pi pi-circle-fill ${classes.green} ${classes.circleFill}`}
+          ></span>
+        </div>
+
+        {props.children}
       </div>
-      
-      {props.children}
-    </div>
+    </Draggable>
   );
 };
 
