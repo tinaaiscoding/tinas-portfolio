4ec93b72a9470fa2425bd4fc304551c84005d09c
tinanvo <tinanhivo@gmail.com>
2023-03-17 20:00:23 +1100

    Fix bug - Resume svg was laggy. Change the svg image to React Component and its no longer lags when opened.

--- src/components/Dockbar/Resume.js
+++ src/components/Dockbar/Resume.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import resume from '../../images/resume.svg'
+// import resume from '../../images/resume.svg'
+import {ReactComponent as ResumeSvg} from '../../images/resume.svg'
 
 import Window from '../UI/Window'
 import classes from './Resume.module.css'
@@ -8,7 +9,8 @@ import classes from './Resume.module.css'
 const Resume = (props) => {
   return (
     <Window className={classes.resume} onCloseWindow={props.onCloseWindow}>
-      <img className={classes.resumeSvg} src={resume} alt="" />
+      {/* <img className={classes.resumeSvg} src={resume} alt="" /> */}
+      <ResumeSvg></ResumeSvg>
     </Window>
   )
 }
